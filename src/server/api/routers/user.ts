4a423162d11399bdb1850863bc87b23a3e1024e5
CarlosELorenzo <ce.lorenzol@gmail.com>
2023-02-01 18:14:02 +0100

    feat: add follow function to the user api

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,7 +1,14 @@
-import { getUser } from "../../../utils/api/user";
+import { follow, getUser, isFollowed, unFollow } from "../../../utils/api/user";
 import { getUserSchema } from "../../../utils/schemas/user";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
   get: publicProcedure.input(getUserSchema).query(getUser),
+  follow: protectedProcedure
+    .input(getUserSchema)
+    .mutation(async ({ ctx, input }) => {
+      if (!(await isFollowed({ ctx, input })))
+        return await follow({ ctx, input });
+      else return unFollow({ ctx, input });
+    }),
 });
--- src/utils/api/user.ts
+++ src/utils/api/user.ts
@@ -19,8 +19,54 @@ export const getUser = async ({ ctx, input }: UserRouterInputs) => {
           followerId: session?.user?.id,
         },
       },
+      _count: {
+        select: {
+          followers: true,
+          follows: true,
+        },
+      },
     },
   });
   return user;
 };
 
+export const isFollowed = async ({ ctx, input }: UserRouterInputs) => {
+  const { prisma, session } = ctx;
+  const { id } = input;
+  const userId = session?.user?.id;
+  const follow = await prisma.follow.findFirst({
+    where: {
+      followerId: userId,
+      followingId: id,
+    },
+  });
+  return !!follow;
+};
+
+export const follow = async ({ ctx, input }: UserRouterInputs) => {
+  const { prisma, session } = ctx;
+  const { id } = input;
+  const userId = session?.user?.id as string;
+  const follow = await prisma.follow.create({
+    data: {
+      followerId: userId,
+      followingId: id,
+    },
+  });
+  return follow;
+};
+
+export const unFollow = async ({ ctx, input }: UserRouterInputs) => {
+  const { prisma, session } = ctx;
+  const { id } = input;
+  const userId = session?.user?.id as string;
+  const follow = await prisma.follow.delete({
+    where: {
+      followerId_followingId: {
+        followerId: userId,
+        followingId: id,
+      },
+    },
+  });
+  return follow;
+};
